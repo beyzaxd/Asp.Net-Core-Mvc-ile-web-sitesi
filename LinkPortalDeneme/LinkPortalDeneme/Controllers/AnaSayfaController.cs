6821ad6f023dce4b0640e6d1437bbd42691232fe
beyzaxd <98029772+beyzaxd@users.noreply.github.com>
2022-09-30 13:27:51 +0300

    Asp .Net Core Mvc ile web sitesi
    Katılım Emeklilik'te geçen staj sürecimde asp.net core mvc kullanarak yaptığım web sitesi.

---
+++ LinkPortalDeneme/LinkPortalDeneme/Controllers/AnaSayfaController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LinkPortalDeneme.Models; //modeli dahil edip context sınıfından nesne türettim
+using Microsoft.Data.SqlClient;
+//using LinkPortalDenemee.Models;
+
+namespace LinkPortalDeneme.Controllers
+{
+    public class AnaSayfaController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            List<DbModal> List = new List<DbModal>();
+
+            SqlConnection cnc = new SqlConnection("Server=SQLTSTSRV02\\SQLGNLTST;Database=LinkPortalTest;User Id=linkportaltstuser;Password=NEBZ*x7wsjmAGp;");
+
+            SqlCommand cmd = new SqlCommand("SELECT lk.AD , lk.Aciklama, lk.Url, l.* FROM Links l, LinkKategori lk WHERE l.KategoriID =lk.ID ", cnc);
+            cnc.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    List.Add(new DbModal { LinkAd = dr["AD"].ToString(), LinkDesc = dr["Aciklama"].ToString(), LinkUrl = dr["Url"].ToString(), ProdUrl = dr["ProdUrl"].ToString(), PreProdUrl = dr["PreProdUrl"].ToString(), TestUrl = dr["TestUrl"].ToString() });
+                }
+            }
+
+
+            
+            dr.Close();
+            cmd.Dispose();
+            cnc.Close();
+
+
+            return View(List);
+        }
+
+        public IActionResult ProdLink()
+        {
+
+            List<DbModal> List = new List<DbModal>();
+            
+
+            SqlConnection cnc = new SqlConnection("Server=SQLTSTSRV02\\SQLGNLTST;Database=LinkPortalTest;User Id=linkportaltstuser;Password=NEBZ*x7wsjmAGp;");
+
+            SqlCommand cmd = new SqlCommand("SELECT lk.AD , lk.Aciklama, lk.Url, l.* FROM Links l, LinkKategori lk WHERE l.KategoriID =lk.ID ", cnc);
+            cnc.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    List.Add(new DbModal { LinkAd = dr["AD"].ToString(), ProdUrl = dr["ProdUrl"].ToString() });
+                }
+            }
+
+
+            dr.Close();
+            cmd.Dispose();
+            cnc.Close();
+
+
+            return View(List);
+        }
+
+        public IActionResult PreProdLink()
+        {
+            List<DbModal> List = new List<DbModal>();
+
+
+            SqlConnection cnc = new SqlConnection("Server=SQLTSTSRV02\\SQLGNLTST;Database=LinkPortalTest;User Id=linkportaltstuser;Password=NEBZ*x7wsjmAGp;");
+
+            SqlCommand cmd = new SqlCommand("SELECT lk.AD , lk.Aciklama, lk.Url, l.* FROM Links l, LinkKategori lk WHERE l.KategoriID =lk.ID ", cnc);
+            cnc.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    List.Add(new DbModal { LinkAd = dr["AD"].ToString(), PreProdUrl = dr["PreProdUrl"].ToString() });
+                }
+            }
+
+            
+            dr.Close();
+            cmd.Dispose();
+            cnc.Close();
+
+
+            return View(List);
+        }
+
+        public IActionResult TestLink()
+        {
+            List<DbModal> List = new List<DbModal>();
+
+
+            SqlConnection cnc = new SqlConnection("Server=SQLTSTSRV02\\SQLGNLTST;Database=LinkPortalTest;User Id=linkportaltstuser;Password=NEBZ*x7wsjmAGp;");
+
+            SqlCommand cmd = new SqlCommand("SELECT lk.AD , lk.Aciklama, lk.Url, l.* FROM Links l, LinkKategori lk WHERE l.KategoriID =lk.ID ", cnc);
+            cnc.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    List.Add(new DbModal { LinkAd = dr["AD"].ToString(), TestUrl = dr["TestUrl"].ToString() });
+                }
+            }
+
+
+            dr.Close();
+            cmd.Dispose();
+            cnc.Close();
+
+
+            return View(List);
+        }
+
+        
+        public IActionResult VeriAl()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VeriAl(DbModal model)
+        {
+
+            DbModal ekle = new DbModal();
+            SqlConnection cnc = new SqlConnection("Server=SQLTSTSRV02\\SQLGNLTST;Database=LinkPortalTest;User Id=linkportaltstuser;Password=NEBZ*x7wsjmAGp;");
+            //SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Form (name,surname,subject,email,message) VALUES (@name,@surname,@subject,@email,@message)", cnc);
+
+            ekle.name=model.name;
+            ekle.surname=model.surname; 
+            ekle.subject=model.subject; 
+            ekle.email=model.email; 
+            ekle.message=model.message;
+
+
+            string query = "INSERT INTO dbo.Form (name,surname,subject,email,message) VALUES ('" + ekle.name + "','" + ekle.surname + "','" + ekle.subject + "','" + ekle.email + "','" + ekle.message + "')";
+            SqlCommand cmd = new SqlCommand(query, cnc);
+
+
+
+            try
+            {
+                //Bağlantımı açıyorum.
+                cnc.Open();
+                //Burada ExcuteNonQuery kullanıyorum, çünkü bana geriye herhangi bir veri listesi geri dönmeyecek.
+                cmd.ExecuteNonQuery();
+                //Komut çalışıp sonlandıktan sonra tekrar aynı sayfaya yönleneceğim.
+                Response.Redirect("Index"); //bu satırı yazmasak da olur
+            }
+            
+            finally
+            {
+                //Bağlantımı kapatıyorum.
+                cnc.Close();
+            }
+
+
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ LinkPortalDeneme/LinkPortalDeneme/Models/DbModal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkPortalDeneme
+{
+    public class DbModal
+    {
+        public int LinkID { get; set; }
+        public string LinkAd { get; set; }
+        public string LinkProd { get; set; }
+        public string LinkPreProd { get; set; }
+        public string LinkTest { get; set; }
+        public string LinkKategoriAD { get; set; }
+        public string LinkOrtamAD { get; set; }
+        public string LinkDesc { get; set; }
+
+        public string LinkUrl { get; set; }
+
+        public string ProdUrl { get; set; }
+        public string PreProdUrl { get; set; }
+        public string TestUrl { get; set; }
+
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string subject { get; set; }
+        public string email { get; set; }
+        public string message { get; set; }
+
+    }
+}
